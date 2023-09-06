d8d6584c3c32f0b48d61f98df34f8fbf0ffce560
babriyabharat <bharatbabriya2005@gmail.com>
2023-09-06 18:10:41 +0530

    first commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -14,6 +14,16 @@ import Service1 from '../Image/Service1.png';
 import Service2 from '../Image/Service2.png';
 import Information1 from '../Image/Information1.png';
 import Information2 from '../Image/Information2.png';
+import Project1 from '../Image/Project1.png';
+import Project2 from '../Image/Project2.png';
+import Project3 from '../Image/Project3.png';
+import Project4 from '../Image/Project4.png';
+import Work1 from '../Image/Work1.png';
+import Team1 from '../Image/Team1.png';
+import Team2 from '../Image/Team2.png';
+import Team3 from '../Image/Team3.png';
+import Footerlogo from '../Image/Footerlogo.png';
+
 
 
 function Home() {
@@ -127,7 +137,7 @@ function Home() {
                                 <span>Our Service</span>
                                 <h1>Amazing Business Services</h1>
                             </div>
-                            <div className='img2 img-fluid w-50'>
+                            <div className='img2 img-fluid w-50 justify-content-center text-center'>
                                 <img src={Service2} alt='Service2' />
                                 <div class="info">
                                     <div class="info-content">
@@ -136,7 +146,7 @@ function Home() {
                                     </div>
                                 </div>
                             </div>
-                            <div className='img1 img-fluid w-50'>
+                            <div className='img1 img-fluid w-50 justify-content-center text-center '>
                                 <img src={Service1} alt='Service1' />
                                 <div class="info">
                                     <div class="info-content">
@@ -169,44 +179,120 @@ function Home() {
                 </section>
                 <section className='project py'>
                     <div className='project-title'>
-                        <span className=''>Latest Project & Case</span>
+                        <span   >Latest Project & Case</span>
                         <h1>Let’s Looks Our Global Projects.</h1>
                     </div>
                     <div className='project-image d-flex'>
-                        <div className='img1 img-fluid w-50 col-3'>
-                            <img src={Service2} alt='Service2' />
+                        <div className='img1 img-fluid col-3 justify-content-center text-center'>
+                            <img src={Project1} alt='Service2' />
                             <div class="pro-title">
                                 <div class="pro-content">
                                     <h2>Technical Analysis</h2>
-                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going</span>
+                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have</span>
                                 </div>
                             </div>
                         </div>
-                        <div className='img2 img-fluid w-50 col-3'>
-                            <img src={Service2} alt='Service2' />
-                            <div class="pro-title">
-                                <div class="pro-content">
+                        <div className='img2 img-fluid col-3 justify-content-center text-center'>
+                            <img src={Project2} alt='Service2' />
+                            <div class="pro-title2">
+                                <div class="pro-content2">
                                     <h2>Technical Analysis</h2>
-                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going</span>
+                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have</span>
                                 </div>
                             </div>
                         </div>
-                        <div className='img3 img-fluid w-50 col-3'>
-                            <img src={Service2} alt='Service2' />
-                            <div class="pro-title">
-                                <div class="pro-content">
+                        <div className='img3 img-fluid col-3 justify-content-center text-center'>
+                            <img src={Project3} alt='Service2' />
+                            <div class="pro-title3">
+                                <div class="pro-content3">
                                     <h2>Technical Analysis</h2>
-                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going</span>
+                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have</span>
                                 </div>
                             </div>
                         </div>
-                        <div className='img4 img-fluid w-50 col-3'>
-                            <img src={Service2} alt='Service2' />
-                            <div class="pro-title">
-                                <div class="pro-content">
+                        <div className='img4 img-fluid col-3 justify-content-center text-center'>
+                            <img src={Project4} alt='Service2' />
+                            <div class="pro-title4">
+                                <div class="pro-content4">
                                     <h2>Technical Analysis</h2>
-                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going</span>
+                                    <span>There are many variations of passages of Lorem Ipsum available, but the majority have</span>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='work'>
+                    <div className='work-sec'>
+                        <div className='work-image'>
+                            <img src={Work1} alt='work1' />
+                        </div>
+                        <div className='work-contant d-flex'>
+                            <div className='work-title col-6'>
+                                <h1>Get Free Consultations! We’re<br /> Ready to Work Together</h1>
+                            </div>
+                            <div className='btn col-6'>
+                                <button type="button">Get Free Consultations</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='team py'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='team-contant d-flex justify-content-between'>
+                                <div className='team-member-1 col-4'>
+                                    <img src={Team1} alt='Team1' />
+                                    <h4 className='p-3 pb-0'>Robert C. Simmons</h4>
+                                    <p className='p-3 pt-0'>Business Consultant</p>
+                                </div>
+                                <div className='team-member-2 col-4'>
+                                    <img src={Team2} alt='Team2' />
+                                    <h4 className='p-3 pb-0'>Christopher L. Wagner</h4>
+                                    <p className='p-3 pt-0'>Senior Manager</p>
+                                </div>
+                                <div className='team-member-3 col-4'>
+                                    <img src={Team3} alt='Team3' />
+                                    <h4 className='p-3 pb-0'>Lawrence C. Dickerson</h4>
+                                    <p className='p-3 pt-0'>Financial Consultant</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='newsletter py'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='newsletter-contant d-flex'>
+                                <div className='col-7 newsletter-title'>
+                                    <h3>Subscribe to Our Newsletter For<br /> Weekly Article Update.</h3>
+                                    <span>Passages of lorem ipsum available, but the majority have suffered<br /> alteration, by injected words.</span>
+                                </div>
+                                <div className='col-2 hr-row d-flex'>
+                                    <div className='vr'></div>
+                                </div>
+                                <div className='btn col-3 d-felx align-items-center'>
+                                    <button type="button" className='px-5'>Get Start</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='footer py'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='footer-contant'>
+                                <div className='footer-logo col-6'>
+                                    <img src={Footerlogo} alt='footerlogo'/>
+                                    <h6 className='py-4'>2021 Award winning Vaccination<br/> and Lorem ipsum dolor sit amet</h6>
+                                </div>
+                                <div className='footer-menu1 col-2'>
+                                    <ul>
+                                    <li><a></a></li>
+                                    
+                                    </ul>
+                                </div>
+                                <div className='footer-menu2 col-2'></div>
+                                <div className='footer-menu3 col-2'></div>
                             </div>
                         </div>
                     </div>
